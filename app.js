80b496c00a13d8d4b7cec109d19d96f0b015eb5d
Ines Ramoul <ines.ramoul@etu.unice.fr>
2018-12-14 11:18:33 +0100

    first commit, intro nodejs and express

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise; //faire une promesse de storage
+mongoose.connect("mongodb://localhost:27017/node-demo");
+var nameSchema = new mongoose.Schema({
+    name: String,
+    job: String
+  });
+
+var User = mongoose.model("User", nameSchema);
+    
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+      .then(item => {
+        res.send("Thank you for filling the form, the data was sent to database.");
+      })
+      .catch(err => {
+        res.status(400).send("unable to save to database");
+      });
+  });
+ 
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+});
