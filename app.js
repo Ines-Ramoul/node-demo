368161f765b107659ccff3d88c513ecd69018733
Ines Ramoul <ines.ramoul@etu.unice.fr>
2018-12-14 16:57:48 +0100

    adding static file assets

--- app.js
+++ app.js
@@ -10,18 +10,42 @@ var mongoose = require("mongoose");
 mongoose.Promise = global.Promise; //faire une promesse de storage
 mongoose.connect("mongodb://localhost:27017/node-demo");
 var nameSchema = new mongoose.Schema({
-    name: String,
-    job: String
+    lastName: String,
+    firstName: String,
+    site: String,
+    metier: String,
+    outil: String,
+    projet: String,
+    programmes: String,
+    task: String,
+    functions: String,
+    funcadd: String,
+    search: String,
+    ecm: String,
+    ecm_freq: String,
+    ecm_nav: String,
+    wiki: String,
+    wiki_freq: String,
+    wiki_nav: String,
+    intranet: String,
+    intranet_freq: String,
+    intranet_nav: String,
+    raison: String,
+    colab: String,
+    but: String,
+    besoin_intranet: String,
   });
 
-var User = mongoose.model("User", nameSchema);
-    
+var Sondage = mongoose.model("Sondage", nameSchema);
+
+app.use('/assets',express.static('assets'));    
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/addname", (req, res) => {
-    var myData = new User(req.body);
+app.post("/formsubmitted", (req, res) => {
+    var myData = new Sondage(req.body);
     myData.save()
       .then(item => {
         res.send("Thank you for filling the form, the data was sent to database.");
